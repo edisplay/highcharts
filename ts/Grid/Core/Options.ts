768f09d8098b7f7e8f5500560d11f750a7a2ef33
Dawid Draguła <46498046+ddragula@users.noreply.github.com>
2026-04-01 10:21:40 +0200

    Update ts/Grid/Core/Options.ts
    Co-authored-by: stitot <totland@gmail.com>

--- ts/Grid/Core/Options.ts
+++ ts/Grid/Core/Options.ts
@@ -210,7 +210,7 @@ export interface Options {
      * Defines the structure of levels in header. Used for grouping columns
      * headers.
      *
-     * Can be an array of strings (column IDs) or an array of objects of type
+     * An array where each item can be either a string (column ID) or an object of type
      * {@link https://api.highcharts.com/grid/typedoc/interfaces/Grid_Core_Options.GroupedHeaderOptions.html | GroupedHeaderOptions}.
      *
      * Try it: {@link https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/grid-lite/basic/grouped-headers | Grouped headers}
