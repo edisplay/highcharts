2018c1b3d61c9369144e172633bfa1b14d641016
magdalena-gut <magdalena.gut@blacklabel.net>
2026-04-21 18:24:50 +0200

    Removed typedef tag for RangeSelectorButtonTypeValue.

--- ts/Stock/RangeSelector/RangeSelector.ts
+++ ts/Stock/RangeSelector/RangeSelector.ts
@@ -2306,12 +2306,6 @@ export default RangeSelector;
  * */
 
 /**
- * Define the time span for the button
- *
- * @typedef {"all"|"day"|"hour"|"millisecond"|"minute"|"month"|"second"|"week"|"year"|"ytd"} Highcharts.RangeSelectorButtonTypeValue
- */
-
-/**
  * Callback function to react on button clicks.
  *
  * @callback Highcharts.RangeSelectorClickCallbackFunction
