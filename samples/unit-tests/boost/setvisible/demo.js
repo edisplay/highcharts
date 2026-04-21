07830a614f1f50a16f1d094afbaed97c94cc162b
Markus Knutson Barstad <markus@Markuss-MacBook-Air.local>
2026-04-21 14:22:22 +0200

    Simplify boost setvisible unit test

--- samples/unit-tests/boost/setvisible/demo.js
+++ samples/unit-tests/boost/setvisible/demo.js
@@ -1,27 +1,14 @@
 QUnit.test('Boosted series show/hide', function (assert) {
-    var chart = Highcharts.chart('container', {
-        boost: {
-            // Do not allow the chart to force chart-wide boosting
-            allowForce: false
-        },
-        plotOptions: {
-            series: {
-                boostThreshold: 1
-            }
-        },
-        series: [
-            {
-                data: [1, 3, 2, 4],
-                visible: false
-            },
-            {
-                data: [4, 2, 5, 3]
-            }
-        ]
-    });
-
-    const s = chart.series[0],
-        blankPixel = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+    const s = Highcharts.chart('container', {
+        series: [{
+            boostThreshold: 1,
+            data: [1, 3],
+            visible: false
+        }, {
+            data: [4, 2]
+        }]
+    }).series[0];
+    const blankPixel = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 
     assert.strictEqual(
         s.boost.target.attr('href'),
@@ -46,19 +33,19 @@ QUnit.test('Boosted series show/hide', function (assert) {
 });
 
 QUnit.test('Boosted and not boosted series - visibility', function (assert) {
-    var chart = Highcharts.chart('container', {
-            series: [
-                {
-                    boostThreshold: 1000,
-                    data: [10, 3]
-                },
-                {
-                    boostThreshold: 1,
-                    data: [5, 10]
-                }
-            ]
-        }),
-        series = chart.series[0];
+    const chart = Highcharts.chart('container', {
+        series: [
+            {
+                boostThreshold: 1000,
+                data: [10, 3]
+            },
+            {
+                boostThreshold: 1,
+                data: [5, 10]
+            }
+        ]
+    });
+    const series = chart.series[0];
 
     series.hide();
     series.show();
